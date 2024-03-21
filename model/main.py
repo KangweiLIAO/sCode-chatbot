a09fffd6682f6a6937019d86d29dcc966d137b40
Kangwei Liao <kangweiliaoo@gmail.com>
2024-03-21 03:30:50 -0400

    Init CodeT5 finetune script and dockerfile

--- model/main.py
+++ model/main.py
@@ -1,96 +1,161 @@
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, Trainer, TrainingArguments, default_data_collator
-from datasets import load_dataset, DatasetDict
-import torch
+"""
+Finetune CodeT5+ models on any Seq2Seq LM tasks
+You can customize your own training data by following the HF dataset format to cache it to args.cache_data
+Author: Yue Wang
+Date: June 2023
+----------------------------------------
+Modified by: Kangwei Liao on Mar. 2024
+"""
 
-model = AutoModelForQuestionAnswering.from_pretrained('deepset/tinyroberta-squad2')
-tokenizer = AutoTokenizer.from_pretrained('deepset/tinyroberta-squad2')
-
-
-def preprocess_function(examples):
-    # Combine 'title' and 'answer' to form the context for each example
-    contexts = [title + " " + answer for title, answer in zip(examples['title'], examples['answer'])]
-    questions = examples['question']
-
-    # Tokenize the question-context pairs
-    # Ensure return_offsets_mapping=True for generating offset mappings
-    tokenized_examples = tokenizer(questions, contexts, truncation=True, padding="max_length", max_length=512,
-                                   return_offsets_mapping=True)
-
-    start_positions = []
-    end_positions = []
-
-    for i, (context, answer) in enumerate(zip(contexts, examples['answer'])):
-        start_char = context.index(answer)
-        end_char = start_char + len(answer)
-
-        # Find the start and end of the answer in the tokenized context
-        start_token = tokenized_examples.char_to_token(i, start_char, sequence_index=1)
-        end_token = tokenized_examples.char_to_token(i, end_char - 1, sequence_index=1)
-
-        # Model needs answer positions in the context
-        start_positions.append(start_token if start_token is not None else 0)
-        end_positions.append(end_token if end_token is not None else 0)
-
-    tokenized_examples["start_positions"] = start_positions
-    tokenized_examples["end_positions"] = end_positions
-
-    # Remove offset_mapping to avoid errors during training
-    del tokenized_examples["offset_mapping"]
-    return tokenized_examples
+import os, re
+import pprint
+import argparse
+from datasets import load_dataset, load_from_disk
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, TrainingArguments, Trainer
 
 
-if torch.cuda.is_available():
-    print(f"GPU is available: {torch.cuda.get_device_name(0)}")
+def run_training(args, model, train_data):
+    print(f"Starting main loop")
 
-    # Load the dataset from a CSV file
-    dataset = load_dataset("KonradSzafer/stackoverflow_python_preprocessed")
-
-    # Apply the preprocessing function
-    tokenized_datasets = dataset.map(preprocess_function, batched=True)
-    split = tokenized_datasets['train'].train_test_split(test_size=0.1)  # Adjust the test_size as needed
-
-    # Creating a DatasetDict to hold the splits for convenience
-    dataset_split = DatasetDict({
-        'train': split['train'],
-        'test': split['test']
-    })
-
-    # Split the dataset into training and validation sets
-    train_dataset = dataset_split["train"].shuffle(seed=42).select(range(1000))  # Example subset for training
-    eval_dataset = dataset_split["test"].shuffle(seed=42).select(range(200))  # Example subset for evaluation
-
-    # Define training arguments
     training_args = TrainingArguments(
-        output_dir="./results",  # output directory for checkpoints and predictions
-        num_train_epochs=30,  # total number of training epochs
-        per_device_train_batch_size=20,  # batch size per device during training
-        warmup_steps=500,  # number of warmup steps for learning rate scheduler
-        weight_decay=0.01,  # strength of weight decay normalization
-        logging_dir="./logs",  # directory for storing logs
-        logging_strategy="epoch",  # log training information every epoch
-        evaluation_strategy="epoch",  # evaluate model every epoch
-        save_strategy="epoch",  # save checkpoints every `logging_steps` steps
-        save_total_limit=3,  # only keep the 2 most recent checkpoints
+        report_to='tensorboard',
+        output_dir=args.save_dir,
+        overwrite_output_dir=False,
+
+        do_train=True,
+        save_strategy='epoch',
+
+        num_train_epochs=args.epochs,
+        per_device_train_batch_size=args.batch_size_per_replica,
+        gradient_accumulation_steps=args.grad_acc_steps,
+
+        learning_rate=args.lr,
+        weight_decay=0.05,
+        warmup_steps=args.lr_warmup_steps,
+
+        logging_dir=args.save_dir,
+        logging_first_step=True,
+        logging_steps=args.log_freq,
+        save_total_limit=1,
+
+        dataloader_drop_last=True,
+        dataloader_num_workers=4,
+
+        local_rank=args.local_rank,
+        deepspeed=args.deepspeed,
+        fp16=args.fp16,
     )
 
-    # Initialize the Trainer
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
-        data_collator=default_data_collator,
-        tokenizer=tokenizer,
+        train_dataset=train_data,
     )
 
-    # Train the model
     trainer.train()
-    trainer.save_model("./model")  # Save the trained model
 
-    # Evaluate the model
-    eval_dict = trainer.evaluate()
-    # Save the evaluation results
-    with open("evaluation.txt", "w") as f:
-        f.write(str(eval_dict))
-else:
-    print("GPU is not available.")
+    if args.local_rank in [0, -1]:
+        final_checkpoint_dir = os.path.join(args.save_dir, "final_checkpoint")
+        model.save_pretrained(final_checkpoint_dir)
+        print(f'  ==> Finish training and save to {final_checkpoint_dir}')
+
+
+def load_tokenize_data(args):
+    # Load and tokenize data
+    if os.path.exists(args.cache_data):
+        train_data = load_from_disk(args.cache_data)
+        print(f'  ==> Loaded {len(train_data)} samples from {args.cache_data}')
+        return train_data
+    else:
+        def filter_entries(examples):
+            # Filter based on scores
+            return examples['question_score'] >= 30 and examples['answer_score'] >= 5
+
+        def clean_text(text):
+            # Function to remove HTML tags and specific whitespace characters
+            clean = re.sub('<.*?>', '', text)  # Remove HTML tags
+            clean = re.sub(r'[\n\t\r]', ' ', clean)  # Replace \n, \t, \r with a space
+            return clean
+
+        def preprocess_function(examples):
+            source = [clean_text(q) for q in examples['question_body']]
+            target = [clean_text(a) for a in examples['answer_body']]
+
+            model_inputs = tokenizer(source, max_length=args.max_source_len, padding="max_length", truncation=True)
+            labels = tokenizer(target, max_length=args.max_target_len, padding="max_length", truncation=True)
+
+            model_inputs["labels"] = labels["input_ids"].copy()
+            model_inputs["labels"] = [
+                [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in model_inputs["labels"]
+            ]
+            return model_inputs
+
+        datasets = load_dataset("koutch/stackoverflow_python", split="train")
+        tokenizer = AutoTokenizer.from_pretrained(args.load)
+
+        datasets = datasets.filter(filter_entries)  # Filter based on scores
+
+        train_data = datasets.map(
+            preprocess_function,
+            batched=True,
+            remove_columns=datasets.column_names,
+            num_proc=64,
+            load_from_cache_file=False,
+        )
+        print(f'  ==> Loaded {len(train_data)} samples from default dataset')
+        train_data.save_to_disk(args.cache_data)
+        print(f'  ==> Saved preprocessed dataset to {args.cache_data}')
+        return train_data
+
+
+def main(args):
+    argsdict = vars(args)
+    print("Applying following arguments:")
+    print(pprint.pformat(argsdict))
+
+    # Save command to file
+    with open(os.path.join(args.save_dir, "command.txt"), 'w') as f:
+        f.write(pprint.pformat(argsdict))
+
+    # Load and tokenize data using the tokenizer from `args.load`. If the data is already cached, load it from there.
+    # You can customize this function to load your own data for any Seq2Seq LM tasks.
+    train_data = load_tokenize_data(args)
+
+    if args.data_num != -1:
+        train_data = train_data.select([i for i in range(args.data_num)])
+
+    # Load model from `args.load`
+    model = AutoModelForSeq2SeqLM.from_pretrained(args.load)
+    print(f"  ==> Loaded model from {args.load}, model size {model.num_parameters()}")
+
+    run_training(args, model, train_data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="CodeT5+ finetuning on Seq2Seq LM task")
+    parser.add_argument('--data-num', default=-1, type=int)
+    parser.add_argument('--max-source-len', default=320, type=int)
+    parser.add_argument('--max-target-len', default=256, type=int)
+    parser.add_argument('--cache-data', default='cache_data/default/', type=str)
+    parser.add_argument('--load', default='Salesforce/codet5p-220m', type=str)
+
+    # Training
+    parser.add_argument('--epochs', default=3, type=int)
+    parser.add_argument('--lr', default=5e-5, type=float)
+    parser.add_argument('--lr-warmup-steps', default=200, type=int)
+    parser.add_argument('--batch-size-per-replica', default=8, type=int)
+    parser.add_argument('--grad-acc-steps', default=4, type=int)
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--deepspeed', default=None, type=str)
+    parser.add_argument('--fp16', default=False, action='store_true')
+
+    # Logging and stuff
+    parser.add_argument('--save-dir', default="saved_models/", type=str)
+    parser.add_argument('--log-freq', default=20, type=int)
+    parser.add_argument('--save-freq', default=500, type=int)
+
+    args = parser.parse_args()
+
+    os.makedirs(args.save_dir, exist_ok=True)
+
+    main(args)
